98c99e1b46ceb40dd1332f02cb46df7bcf2060d8
rajeevudm <36358700+rajeevudm@users.noreply.github.com>
2023-04-19 14:11:31 +0400

    First commit of et in pc

---
+++ menu.php
@@ -0,0 +1,187 @@
+ <!-- Header -->
+  <header id="header" class="header">
+    <div class="header-top bg-theme-colored sm-text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="widget no-border m-0">
+              <ul class="styled-icons icon-dark icon-circled icon-theme-colored icon-sm sm-text-center mt-sm-15">
+                <li><a href="#"><i class="fa fa-facebook text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram text-white"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin text-white"></i></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div id="side-panel-trigger" class="side-panel-trigger pull-right flip sm-pull-none mt-5"><a href="#"><i class="fa fa-bars font-24 text-white"></i></a></div>
+            <div class="widget no-border m-0">
+              <ul class="list-inline pull-right flip sm-pull-none sm-text-center mt-5">
+                <li>
+                  <a href="#" class="text-white">Help Desk</a>
+                </li>
+                <li class="text-white">|</li>
+                <li>
+                  <a href="#" class="text-white"> Emergency Services </a>
+                </li>
+                <li class="text-white">|</li>
+                <li>
+                  <a class="text-white ajaxload-popup" href="ajax-load/form-appointment.html">Appointment</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="header-middle p-0 bg-lightest xs-text-center">
+      <div class="container pt-0 pb-0">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4 col-md-5">
+            <div class="widget no-border m-0">
+              <a class="menuzord-brand pull-left flip xs-pull-center mb-15" href="javascript:void(0)"><img src="images/logo-wide.png" alt=""></a>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-4">
+            <div class="widget no-border pull-right sm-pull-none sm-text-center mt-10 mb-10 m-0">
+              <ul class="list-inline">
+                <li><i class="fa fa-mobile text-theme-colored font-42 mt-5 sm-display-block"></i></li>
+                <li>
+                  <a href="#" class="font-12 text-gray text-uppercase">Call us today!</a>
+                  <h5 class="font-14 m-0"> +(012) 345 6789</h5>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-3">
+            <div class="widget no-border pull-right sm-pull-none sm-text-center mt-10 mb-10 m-0">
+              <ul class="list-inline">
+                <li><i class="fa fa-clock-o text-theme-colored font-36 mt-5 sm-display-block"></i></li>
+                <li>
+                  <a href="#" class="font-12 text-gray text-uppercase">We are open!</a>
+                  <h5 class="font-13 text-black m-0"> Mon-Fri 8:00-16:00</h5>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="header-nav">
+      <div class="header-nav-wrapper navbar-scrolltofixed bg-light">
+        <div class="container">
+          <nav id="menuzord" class="menuzord blue bg-light">
+            <ul class="menuzord-menu">
+              <li class="active"><a href="#home">Home</a>
+                <ul class="dropdown">
+                  <li><a href="#">Bel Air Campus</a>
+                    <ul class="dropdown">
+                      <li><a href="index-mp-layout1.html">Layout1</a></li>
+                      <li><a href="index-mp-layout2.html">Layout2</a></li>
+                      <li><a href="index-mp-layout3.html">Layout3</a></li>
+                      <li><a href="index-mp-layout4.html">Layout4</a></li>
+                      <li><a href="index-mp-layout5.html">Layout5</a></li>
+                      <li><a href="index-mp-layout6.html">Layout6</a></li>
+                      <li><a href="index-mp-layout7.html">Layout7</a></li>
+                      <li><a href="index-mp-layout8.html">Layout8</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Department of Emerging Technologies</a>
+                    <ul class="dropdown">
+                      <li><a href="index-sp-layout1.html">Layout1</a></li>
+                      <li><a href="index-sp-layout2.html">Layout2</a></li>
+                      <li><a href="index-sp-layout3.html">Layout3</a></li>
+                      <li><a href="index-sp-layout4.html">Layout4</a></li>
+                      <li><a href="index-sp-layout5.html">Layout5</a></li>
+                      <li><a href="index-sp-layout6.html">Layout6</a></li>
+                      <li><a href="index-sp-layout7.html">Layout7</a></li>
+                      <li><a href="index-sp-layout8.html">Layout8</a></li>
+                    </ul>
+                  </li>
+                  
+                </ul>
+              </li>
+              <li><a href="#">Programmes</a>
+                <ul class="dropdown">
+                  <li><a href="features-preloader.html">Data science and IoT Engineering</a></li>
+                  <li><a href="features-preloader.html">Intelligent Systems Engineering</a></li>
+                  <li><a href="features-preloader.html">BioTechnology Engineering</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Research</a>
+                <ul class="dropdown">
+                  <li><a href="#">Publications</a>
+                    
+                  </li>           
+                  <li><a href="#">Workshops</a>
+                    
+                  </li>
+                  <li><a href="page-timetable.html">Consultancy</a></li>
+                  <li><a href="creetiv_intro.php">Laboratory</a>
+                    
+                  </li>
+                  
+                </ul>
+              </li>           
+              <li><a href="#">Projects</a>
+                <ul class="dropdown">
+                  <li><a href="page-services-details.html">SCCP</a></li>
+                  <li><a href="page-services-details.html">SSC</a></li>
+                  <li><a href="page-services-details.html">FESTII</a></li>
+                  
+                </ul>
+              </li>
+              <li><a href="#">Academics</a>
+                <ul class="dropdown">
+                  
+                  <li><a href="staffs.php">Staffs</a></li>
+                 
+                </ul>
+              </li>
+              <li><a href="#">Short courses</a>
+                <ul class="dropdown">
+                  <li><a href="#">Introduction to IoT</a>
+                      <!--
+                    <ul class="dropdown">
+                      <li><a href="blog-classic-no-sidebar.html">Blog Classic No Sidebar</a></li>
+                      <li><a href="blog-classic-left-sidebar.html">Blog Classic Left Sidebar</a></li>
+                      <li><a href="blog-classic-right-sidebar.html">Blog Classic Right Sidebar</a></li>
+                      <li><a href="blog-classic-both-sidebar.html">Blog Classic Both Sidebar</a></li>
+                      <li><a href="blog-classic-left-thumbs.html">Blog Classic Left Thumbs</a></li>
+                    </ul>
+                      -->
+                  </li>
+                  <li><a href="#">Mobile Application Development</a>
+                    
+                  </li>
+                 
+                  
+                </ul>
+              </li>
+              <li><a href="#">Community</a>
+                <ul class="dropdown">
+                      <li><a href="blog-masonry-2-column.html">Health Day</a></li>
+                      <li><a href="blog-masonry-3-column.html">IT WORKS</a></li>
+                      <li><a href="blog-masonry-4-column.html">ITFA</a></li>
+                    </ul>
+              </li>
+            </ul>
+            <div class="pull-right flip hidden-sm hidden-xs">
+              <div class="widget no-border m-0">
+                <div class="search-form-wrapper">
+                  <form id="searchform" class="m-0 mt-10" method="get">
+                    <input class="" type="text" name="s" id="searchinput" data-height="38px" value="Enter your search" onblur="if(this.value == '') { this.value ='Enter your search'; }" onfocus="if(this.value =='Enter your search') { this.value = ''; }"  >
+                    <label>
+                      <input type="submit" value="" name="submit" data-height="34px">
+                    </label>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  
